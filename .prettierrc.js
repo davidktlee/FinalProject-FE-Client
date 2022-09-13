2de89bc9348c3c142373c65bfbdbe8e61a47f7a6
chan <cham9994@gmail.com>
2022-09-13 14:42:23 +0900

    config: 기본 세팅

---
+++ .prettierrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  semi: false,
+  trailingComma: 'none',
+  singleQuote: true,
+  printWidth: 110,
+  tabWidth: 2,
+  endOfLine: 'auto',
+  bracketSpacing: true,
+}
+
+// {
+//   "arrowParens": "always",
+//   "bracketSpacing": true,
+//   "embeddedLanguageFormatting": "auto",
+//   "htmlWhitespaceSensitivity": "css",
+//   "insertPragma": false,
+//   "jsxBracketSameLine": false,
+//   "jsxSingleQuote": false,
+//   "proseWrap": "preserve",
+//   "quoteProps": "as-needed",
+//   "requirePragma": false,
+//   "semi": true,
+//   "singleQuote": false,
+//   "tabWidth": 2,
+//   "trailingComma": "es5",
+//   "useTabs": false,
+//   "vueIndentScriptAndStyle": false,
+//   "printWidth": 100
+// }
