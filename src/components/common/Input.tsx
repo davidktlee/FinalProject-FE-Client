6416c8ba8797214755c6c7cef55c2ba76afb6556
Kim Myungseong <forwarm5891@gmail.com>
2022-09-17 19:08:55 +0900

    chore: 글자간격,해더배경색,font-family 변경

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -20,10 +20,10 @@ const Input = ({label,placeholder,type,double, flexDirection,type2,placeholder2,
   if(double){
     return (
       <div className='flex flex-col justify-center my-[30px] w-full h-fit gap-2'>
-       <label><span className={`text-${labelColor} font-${labelBold} `} >{label}</span>{' '}<span className='text-rose-400'>{isRequired && '*'}</span></label>
+       <label><span className={`text-${labelColor} font-${labelBold}  font-semibold`} >{label}</span>{' '}<span className='text-rose-400'>{isRequired && '*'}</span></label>
        <div className={`${flexDirection === 'horizontal' ? 'flex items-center justify-start gap-x-4' : 'flex flex-col justify-center gap-y-2'}`}>
-       <input className={`grow h-${inputHeight} border border-solid border-gray-200 rounded-xl max-w-${inputWidth} pl-2 focus:outline-1 focus:outline-[#ABC8DF] placeholder-gray-400/60`} type={type} placeholder={placeholder} disabled={readonly} />
-       <input className={`grow h-${inputHeight} border border-solid border-gray-200 rounded-xl max-w-${inputWidth} pl-2 focus:outline-1 focus:outline-[#ABC8DF] placeholder-gray-400/60`} type={type2} placeholder={placeholder2} />
+       <input className={`grow h-${inputHeight} border border-solid border-gray-200 rounded-md max-w-${inputWidth} pl-2 focus:outline-1 focus:outline-[#ABC8DF] placeholder-gray-400/60`} type={type} placeholder={placeholder} disabled={readonly} />
+       <input className={`grow h-${inputHeight} border border-solid border-gray-200 rounded-md max-w-${inputWidth} pl-2 focus:outline-1 focus:outline-[#ABC8DF] placeholder-gray-400/60`} type={type2} placeholder={placeholder2} />
        </div>
       </div>
     );  
@@ -31,9 +31,9 @@ const Input = ({label,placeholder,type,double, flexDirection,type2,placeholder2,
   if(!double){
     return (
       <div className='flex flex-col justify-center my-4 gap-2'>
-       <label><span className={`text-${labelColor} font-${labelBold}`}>{label}</span>{' '}<span className='text-rose-400'>{isRequired && '*'}</span></label>
+       <label><span className={`text-${labelColor} font-${labelBold} font-semibold`}>{label}</span>{' '}<span className='text-rose-400'>{isRequired && '*'}</span></label>
        <div className={`${flexDirection === 'horizontal' ? 'flex items-center justify-start' : 'flex flex-col justify-center gap-y-2'}`}>
-       <input className={`w-full h-${inputHeight} border border-solid border-gray-200 rounded-xl max-w-${inputWidth} pl-2 focus:outline-1 focus:outline-[#ABC8DF] placeholder-gray-400/60`} type={type}  placeholder={placeholder} />
+       <input className={`w-full h-${inputHeight} border border-solid border-gray-200 rounded-md max-w-${inputWidth} pl-2 focus:outline-1 focus:outline-[#ABC8DF] placeholder-gray-400/60`} type={type}  placeholder={placeholder} />
        </div>
       </div>
       )  
