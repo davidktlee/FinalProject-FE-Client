0c996a4ec64acb29691e9cf80309a92bbd011be2
Kim Myungseong <forwarm5891@gmail.com>
2022-09-18 20:50:40 +0900

    feat: signin  submit 버튼 disabled 기능 및 로직추가

--- src/components/auth/signin/Signin.tsx
+++ src/components/auth/signin/Signin.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent, useState,useEffect } from 'react';
 import Input from '../../common/Input';
 import { VALIDATOR_EMAIL, VALIDATOR_MAXLENGTH, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../hooks/validator';
 
@@ -14,7 +14,7 @@ const Signin = () => {
     email:'',
     password:''
   })
-
+  const [totalValid,setTotalValid] = useState(false)
   const onChange = (e:ChangeEvent<HTMLInputElement>) => {
     const {currentTarget:{value,name}} = e;
     setFormValue(prev => ({
@@ -24,8 +24,31 @@ const Signin = () => {
   }
   const submitSigninHandler = () => {
   console.log(formValue)
-  
   }
+
+  const totalvalidate = () => {
+    
+    let totalValidation = true;
+    
+    const formValueArray = Object.values(formValue);
+    formValueArray.map((item) => {
+      const {password,email} = formValue;
+      if(!item){
+        totalValidation = false;
+        return;
+      }
+      if(email.trim().length < 8 || !/^\S+@\S+\.\S+$/.test(email) || password.length < 10){
+        totalValidation = false
+        return;
+      }
+    })
+    setTotalValid(totalValidation)
+  }
+
+  useEffect(() => {
+    totalvalidate()
+  }, [formValue])
+  
   return (
     <div className='w-full h-screen bg-[#F4F6F8] relative'>
       <div className='absolute top-[36%] left-1/2 -translate-x-1/2 -translate-y-1/2 w-[500px] h-fit mt-[120px] bg-white rounded-lg shadow-[-5px_0_30px_1px] shadow-indigo-100 pb-12 border border-solid border-gray-200'>
@@ -40,7 +63,7 @@ const Signin = () => {
 
         <Input name="password" errorText='비밀번호는 10~20자 입니다.' validators={[VALIDATOR_MINLENGTH(9),VALIDATOR_MAXLENGTH(21)]} value={formValue.password} onChange={onChange} label="Password" placeholder='Password' type="password" double={false} flexDirection="horizontal" isRequired={false} inputWidth={'full'}  inputHeight="12" labelColor='gray-400' labelBold='bold' />
         
-        <button className='bg-[#A4CBE1] w-full h-12 rounded-lg mt-7' onClick={submitSigninHandler}><span className='text-white font-bold'>Sign in</span></button>
+        <button className='bg-[#A4CBE1] w-full h-12 rounded-lg mt-7 disabled:bg-gray-400 disabled:cursor-default' onClick={submitSigninHandler} disabled={!totalValid}><span className='text-white font-bold' >Sign in</span></button>
         </div>
         
       </div>
