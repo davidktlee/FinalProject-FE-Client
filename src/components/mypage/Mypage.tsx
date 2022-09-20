76258766f55a58254c4767a8ffb2f687adc3bf5b
Kim Myungseong <forwarm5891@gmail.com>
2022-09-21 04:59:35 +0900

    feat: 배송 조회 컴포넌트 구현

--- src/components/mypage/Mypage.tsx
+++ src/components/mypage/Mypage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link, NavLink, Outlet, Route, Router, Routes } from 'react-router-dom';
 import PageLayout from '../common/ui/PageLayout';
 import MypageBanner from './MypageBanner';
+import SideNavBar from './SideNavBar';
 
 const Mypage = () => {
 
@@ -11,15 +12,7 @@ const Mypage = () => {
       <div className='w-[90%] mx-auto'>
       <MypageBanner />
       <div className='flex mt-8'>
-        <ul className='flex flex-col items-start justify-start w-[20%] gap-y-1 text-[#7a7a7a] text-sm'>
-          <li><NavLink className={({isActive}) => (isActive ? 'text-[#1B304A] font-bold' : 'text-inherit')} to="myorder">주문 내역</NavLink></li>
-          <li><NavLink className={({isActive}) => (isActive ? 'text-[#1B304A] font-bold' : 'text-inherit')} to="mytracking">배송 조회</NavLink></li>
-          <li><NavLink className={({isActive}) => (isActive ? 'text-[#1B304A] font-bold' : 'text-inherit')} to="myafter">취소/교환/반품 내역</NavLink></li>
-          <li><NavLink className={({isActive}) => (isActive ? 'text-[#1B304A] font-bold' : 'text-inherit')} to="myreview">리뷰 관리</NavLink></li>
-          <li><NavLink className={({isActive}) => (isActive ? 'text-[#1B304A] font-bold' : 'text-inherit')} to="myprofile">회원 정보 수정</NavLink></li>
-          <li><NavLink className={({isActive}) => (isActive ? 'text-[#1B304A] font-bold' : 'text-inherit')} to="mygrade">회원 등급</NavLink></li>
-          <li><NavLink className={({isActive}) => (isActive ? 'text-[#1B304A] font-bold' : 'text-inherit')} to="mycoupon">쿠폰함</NavLink></li>
-        </ul>
+        <SideNavBar />
       <div className='grow'>
       <Outlet />
       </div>
--- src/store/product.ts
+++ src/store/product.ts
@@ -9,6 +9,7 @@ export interface Product {
   price:string
   orderedAt:string
   orderNumber:number
+  shippingStatus:string
 }
 
 export const productState = atom<Product[]>({
@@ -23,7 +24,8 @@ export const productState = atom<Product[]>({
       option:['그래픽 직경 13.5mm','도수: 0.7','수량: 1개'],
       price: '18000원',
       orderedAt:'2022-05-17',
-      orderNumber:1024214524
+      orderNumber:1024214524,
+      shippingStatus:'배송중'
     },
     {
       id:2,
@@ -33,7 +35,8 @@ export const productState = atom<Product[]>({
       option:['그래픽 직경 13.2mm','도수: 0.4','수량: 2개'],
       price: '36000원',
       orderedAt:'2022-03-24',
-      orderNumber:103133324
+      orderNumber:103133324,
+      shippingStatus:'배송완료'
     },
     {
       id:3,
@@ -43,7 +46,8 @@ export const productState = atom<Product[]>({
       option:['그래픽 직경 12.3mm','도수: 0.5','수량: 1개'],
       price: '18000원',
       orderedAt:'2022-09-01',
-      orderNumber:1024214524
+      orderNumber:1024214524,
+      shippingStatus:'입고 대기 중'
     },
     
   ]
