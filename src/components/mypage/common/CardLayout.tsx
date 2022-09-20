aa70963f8842a63d126d67e77a48f21e1b716963
Kim Myungseong <forwarm5891@gmail.com>
2022-09-21 04:19:43 +0900

    feat: tabs: 주문 내역 컴포넌트 구현 및 공통 layout 구현

--- src/components/mypage/common/CardLayout.tsx
+++ src/components/mypage/common/CardLayout.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const CardLayout = () => {
+
+interface CardLayoutProps {
+  title: string;
+  children:React.ReactNode
+}
+
+
+const CardLayout = ({title,children}:CardLayoutProps) => {
   return (
-    <div>
-      
+    <div className=''>
+      <h3 className='font-bold text-[22px] text-[#1B304A] pb-2 border-b-4 border-solid border-[#abc8df]'>{title}</h3>
+     {children}
     </div>
   );
 };
