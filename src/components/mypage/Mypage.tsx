6aaf5f4febfdc9337bdbc636775a6ec043b8facb
Kim Myungseong <forwarm5891@gmail.com>
2022-09-21 03:07:13 +0900

    feat: 마이페이지 배너컴포넌트 퍼블리싱

--- src/components/mypage/Mypage.tsx
+++ src/components/mypage/Mypage.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import PageLayout from '../common/ui/PageLayout';
+import MypageBanner from './MypageBanner';
 
 const Mypage = () => {
   return (
-    <div>
-      
-    </div>
+    <PageLayout title='마이페이지' isTitleVisible={true} layoutWidth="[90%]">
+      <div className='w-[90%] mx-auto'>
+      <MypageBanner />
+      </div>
+    </PageLayout>
   );
 };
 
