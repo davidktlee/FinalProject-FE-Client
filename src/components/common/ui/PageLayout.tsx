b83ff97eda252a07fcad6c7d1df6e02a25983ed4
chan <cham9994@gmail.com>
2022-09-22 02:19:08 +0900

    feat: MainReview 제작

--- src/components/common/ui/PageLayout.tsx
+++ src/components/common/ui/PageLayout.tsx
@@ -1,40 +1,22 @@
 import React, { ReactNode } from 'react'
 
 interface PageLayoutProps {
-<<<<<<< HEAD
-  title: string
   children: React.ReactNode
   layoutWidth: string
-  isTitleVisible?: boolean
-}
 
-const PageLayout = ({ title, children, layoutWidth, isTitleVisible }: PageLayoutProps) => {
+  innerTop?: string
+}
+// shadow-[0_0_6px] bg-white rounded-lg p-10 mt-8  shadow-gray-400/80
+const PageLayout = ({ children, layoutWidth, innerTop }: PageLayoutProps) => {
   return (
     <div className="w-full h-[1846px] bg-[#F4F6F8] text-base ">
       <div
-        className={`absolute top-[50%] left-1/2 -translate-x-1/2 -translate-y-[15%] w-${
-          layoutWidth || 'full'
-        } h-fit bg-white rounded-lg p-10 mt-8 shadow-[0_0_6px] shadow-gray-400/80`}
+        className={`absolute ${
+          innerTop ? innerTop : 'top-1/2'
+        } left-1/2 -translate-x-1/2 -translate-y-[15%] w-${layoutWidth || 'full'} h-fit `}
       >
-        {isTitleVisible && (
-          <h3 className="text-[#1B304A] font-bold text-[22px] text-center w-full py-[25px]">{title}</h3>
-        )}
         {children}
       </div>
-=======
-  
-  children:React.ReactNode
-  layoutWidth:string
-  
-  innerTop?:string
-}
-  // shadow-[0_0_6px] bg-white rounded-lg p-10 mt-8  shadow-gray-400/80
-const PageLayout = ({children,layoutWidth,innerTop}:PageLayoutProps) => {
-  return (
-    <div className="w-full h-[1846px] bg-[#F4F6F8] text-base ">
-    <div className={`absolute ${innerTop ? innerTop : 'top-1/2'} left-1/2 -translate-x-1/2 -translate-y-[15%] w-${layoutWidth || 'full'} h-fit `}>
-      {children}
->>>>>>> 76ec295b98e91f535fdde9ade3e622c62fc320be
     </div>
   )
 }
--- src/components/main/MainReviewCard.tsx
+++ src/components/main/MainReviewCard.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 
 const MainReviewCard = () => {
   return (
-    <div>
+    <div className=" p-[10px] rounded-xl shadow-[0_0_6px] shadow-gray-400/80">
       <div>
-        <img src="" alt="" />
+        <img
+          className=" rounded rounded-t-[5px]"
+          src="https://user-images.githubusercontent.com/90392240/191562901-054caaf5-0245-44fd-8715-848853dcca4e.png"
+        />
       </div>
-      <div>
-        <p>타이틀</p>
-        <p>아이디</p>
+      <div className="p-2">
+        <p>샌드 플러스 그레이 착용</p>
+        <p className="text-[#7F7F7F]">@velvet.ineffable</p>
         <p>★★★★★</p>
       </div>
     </div>
