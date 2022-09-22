98bb3cfce5052b5be34c8249467716fb244c4c9c
Kim Myungseong <forwarm5891@gmail.com>
2022-09-23 04:35:55 +0900

    design: MyOrder 컴포넌트 미디어쿼리 적용

--- src/components/common/ui/PageLayout.tsx
+++ src/components/common/ui/PageLayout.tsx
@@ -6,7 +6,7 @@ interface PageLayoutProps {
 
   innerTop?: string
 }
-// shadow-[0_0_6px] bg-white rounded-lg p-10 mt-8  shadow-gray-400/80
+
 const PageLayout = ({ children, layoutWidth, innerTop }: PageLayoutProps) => {
   return (
     <div className="w-full h-[1846px] bg-[#F4F6F8] text-base ">
