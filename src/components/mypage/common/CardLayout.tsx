d0ed0f037bce662c94174091cae107766765a582
Kim Myungseong <forwarm5891@gmail.com>
2022-09-23 04:19:12 +0900

    design: MyAfter 컴포넌트 미디어 쿼리 적용

--- src/components/mypage/common/CardLayout.tsx
+++ src/components/mypage/common/CardLayout.tsx
@@ -9,7 +9,7 @@ interface CardLayoutProps {
 
 const CardLayout = ({title,children}:CardLayoutProps) => {
   return (
-    <div className='mt-4'>
+    <div className=''>
       <h3 className='hidden xs:block font-bold text-[22px] text-[#1B304A] pb-2 border-b-4 border-solid border-[#abc8df]'>{title}</h3>
      {children}
     </div>
