9addd854ac98d2dc296b1699b68dcfbb2e50c96d
chan <cham9994@gmail.com>
2022-09-25 20:48:12 +0900

    feat: #73 제품 상세 모바일

--- src/components/ProductDetail/ProductInfo.tsx
+++ src/components/ProductDetail/ProductInfo.tsx
@@ -4,6 +4,7 @@ const ProductInfo = () => {
   return (
     <div>
       <img
+        className="mx-auto"
         src="https://user-images.githubusercontent.com/90392240/190920313-0030c7c6-c0b6-40e9-b8d0-a3ea52d8217b.png"
         alt=""
       />
