35d3336d6279215155536370883ad5e581d67124
Kim Myungseong <forwarm5891@gmail.com>
2022-09-27 16:05:19 +0900

    design: payment의 cardtemplate layout 수정

--- src/components/common/ui/CardTemplate.tsx
+++ src/components/common/ui/CardTemplate.tsx
@@ -5,12 +5,13 @@ interface CardTemplateProps {
   children:React.ReactNode
   title:string;
   isTitleVisible?:boolean
+  marginTop?:string
 }
 
-const CardTemplate = ({children,isTitleVisible,title}:CardTemplateProps) => {
+const CardTemplate = ({children,isTitleVisible,title,marginTop}:CardTemplateProps) => {
   return (
-    <div className='shadow-[0_0_6px] bg-white rounded-lg p-2 xs:p-10 shadow-gray-400/80 mt-10'>
-      {isTitleVisible && <h3 className="text-[#1B304A] font-bold text-[22px] text-center w-full py-[25px]">{title}</h3>}
+    <div className={`shadow-[0_0_6px] bg-white rounded-lg p-2 xs:p-10 shadow-gray-400/80 ${marginTop ? marginTop : 'mt-40'}`}>
+      {isTitleVisible && <h3 className="text-[#1B304A] font-bold text-[22px] text-center w-full py-[25px] mt-10">{title}</h3>}
       {children}
     </div>
   );
