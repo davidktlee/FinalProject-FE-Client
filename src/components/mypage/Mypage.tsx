5bf248f7b23a71dafeb63017404d0c56fa365e50
Kim Myungseong <forwarm5891@gmail.com>
2022-09-28 12:38:59 +0900

    chore: useEffect로 active menu 활성화 수정

--- src/components/mypage/Mypage.tsx
+++ src/components/mypage/Mypage.tsx
@@ -7,6 +7,7 @@ import CardTemplate from '../common/ui/CardTemplate'
 import PageLayout from '../common/ui/PageLayout'
 
 import { getStoredToken } from '../local-storage/userStorage'
+import MobileIndexNavBar from './MobileIndexNavBar'
 import MobileNavBar from './MobileNavBar'
 import MypageBanner from './MypageBanner'
 import SideNavBar from './SideNavBar'
@@ -45,26 +46,7 @@ const Mypage = () => {
             </div>
           </div>
         </div>
-        {pathname === '/mypage' && (
-        <div className=' block xs:hidden'>
-          <ul className='flex flex-col items-start text-[#7a7a7a] font-semibold text-sm mr-0 xs:mr-4 min-w-[116px] gap-y-4'>
-            <li className='w-full pb-2 border-b border-solid border-gray-400'><NavLink className={({isActive}) => (isActive ? 'text-[#1B304A]' : 'text-inherit')} to="myorder">주문 내역</NavLink></li>
-            <li className='w-full pb-2 border-b border-solid border-gray-400'><NavLink className={({isActive}) => (isActive ? 'text-[#1B304A]' : 'text-inherit')} to="mytracking">배송 조회</NavLink></li>
-            <li className='w-full pb-2 border-b border-solid border-gray-400'><NavLink className={({isActive}) => (isActive ? 'text-[#1B304A]' : 'text-inherit')} to="myafter">취소/교환/반품 내역</NavLink></li>
-            <li className='w-full pb-2 border-b border-solid border-gray-400'><NavLink className={({isActive}) => (isActive ? 'text-[#1B304A]' : 'text-inherit')} to="myreview">리뷰관리</NavLink></li>
-            <li className='w-full pb-2 border-b border-solid border-gray-400'><NavLink className={({isActive}) => (isActive ? 'text-[#1B304A]' : 'text-inherit')} to="myprofile">회원 정보 수정</NavLink></li>
-
-            
-            <li className='w-full text-sm font-normal pl-2 pb-2 border-b border-solid border-gray-400'><NavLink className='text-inherit' to="myprofile/editprofile">프로필 수정</NavLink></li>
-            <li className='w-full text-sm font-normal pl-2 pb-2 border-b border-solid border-gray-400'><NavLink className='text-inherit' to="myprofile/editsecret">비밀번호 수정</NavLink></li>
-            
-            
-            <li className='w-full pb-2 border-b border-solid border-gray-400'><NavLink className={({isActive}) => (isActive ? 'text-[#1B304A]' : 'text-inherit')} to="mygrade">회원등급</NavLink></li>
-            <li className='w-full pb-2 border-b border-solid border-gray-400'><NavLink className={({isActive}) => (isActive ? 'text-[#1B304A]' : 'text-inherit')} to="mycoupon">쿠폰함</NavLink></li>
-      
-        </ul>
-          </div>
-          )}
+        {pathname === '/mypage' && <MobileIndexNavBar />}
       </CardTemplate>
     </PageLayout>
   )
