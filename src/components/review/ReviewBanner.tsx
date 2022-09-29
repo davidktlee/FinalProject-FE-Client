c8af021b5be11775b5da4f7b0aae741c1ad22754
chan <cham9994@gmail.com>
2022-09-29 18:23:33 +0900

    feat: 리뷰페이지 - 모바일, 웹 스타일링 조정 및 이미지 더미 입력

--- src/components/review/ReviewBanner.tsx
+++ src/components/review/ReviewBanner.tsx
@@ -43,12 +43,12 @@ const ReviewBanner = () => {
   }, [swiperSetting])
 
   const reviewImgs = [
-    'https://dummyimage.com/177.5x175/000/fff',
-    'https://dummyimage.com/177.5x175/000/fff',
-    'https://dummyimage.com/177.5x175/000/fff',
-    'https://dummyimage.com/177.5x175/000/fff',
-    'https://dummyimage.com/177.5x175/000/fff',
-    'https://dummyimage.com/177.5x175/000/fff'
+    'https://user-images.githubusercontent.com/90392240/192992410-a6215ba1-b464-4c3b-ba32-c961ff1468ac.png',
+    'https://user-images.githubusercontent.com/90392240/192993730-42fa0211-0cc7-4b7a-8032-f53d6166b032.png',
+    'https://user-images.githubusercontent.com/90392240/192992448-24080e81-e4fd-432a-9995-be1369e4ee1a.png',
+    'https://user-images.githubusercontent.com/90392240/192992466-2113cc74-e9ca-4c00-9a31-c435276a06c8.png',
+    'https://user-images.githubusercontent.com/90392240/192992483-8154bb2b-7315-4a27-a37c-ee587767cc1a.png',
+    'https://user-images.githubusercontent.com/90392240/192992502-124b6e7f-1f96-4b67-81f8-591bd729bd09.png'
   ]
 
   return (
--- src/components/review/ReviewContainer.tsx
+++ src/components/review/ReviewContainer.tsx
@@ -4,9 +4,9 @@ import RevieItems from './RevieItems'
 const ReviewContainer = () => {
   return (
     <div className="pt-10 pb-4 xs-max:pt-4">
-      <div className="flex justify-between xs-max:flex-col">
+      <div className="flex flex-col">
         <p className="">60개의 리뷰</p>
-        <div className="xs:hidden divider h-[1px] bg-[#dadada] my-2"></div>
+        <div className="xs-max:hidden divider h-[1px] bg-[#dadada] my-1 w-full"></div>
       </div>
       <section className="text-gray-600 body-font bg-[]">
         <div className="container px-4 mx-auto flex-wrap">
