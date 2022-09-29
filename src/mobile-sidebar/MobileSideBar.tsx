2382ac4027f92a9e2853405e2940308f4a299e22
Kim Myungseong <forwarm5891@gmail.com>
2022-09-29 20:24:12 +0900

    chore: 모바일 푸터 글자 크기 조정

--- src/mobile-sidebar/MobileSideBar.tsx
+++ src/mobile-sidebar/MobileSideBar.tsx
@@ -73,18 +73,18 @@ const MobileSideBar = () => {
           <li className='pl-2 pr-4 pb-2 w-full '><Link onClick={pageChangeHandler} className='flex justify-between' to="review"><span>Review</span><span><img src="/assets/rightarrow.png" alt="" /></span></Link></li>
         </ul>
 
-
+{/* footer */}
         <div className='text-lenssisGray pl-6 pt-4'>
         <ul className='w-full mx-auto flex flex-col items-start xs:items-center xs:justify-center gap-y-1 '>
-      <li className='font-[900] pb-2 text-2xl tracking-tight'>LENSSIS</li>
-      <li>주식회사 <span className='font-bold'>LENSSIS</span></li>
-      <li>전화번호 <span className='font-bold'>050-3558-4887</span></li>
-      <li>문의시간 <span className='font-bold'>평일 10:00 17:30</span></li>
+      <li className='font-[900] pb-2 text-xl tracking-tight'>LENSSIS</li>
+      <li className='text-sm'>주식회사 <span className='font-bold'>LENSSIS</span></li>
+      <li className='text-sm'>전화번호 <span className='font-bold'>050-3558-4887</span></li>
+      <li className='text-sm'>문의시간 <span className='font-bold'>평일 10:00 17:30</span></li>
     </ul>
     <ul className='flex flex-col items-star xs:flex-row xs:justify-center xs:items-center pb-20 xs:gap-x-10 w-full mt-4'>
-      <li className='text-sm border-b xs:border-b-0 border-solid w-fit'><Link to="/">이용 규약과 정책</Link></li>
-      <li className='text-sm border-b xs:border-b-0 border-solid w-fit'><Link to="/">특정 상거래에 관한 볍률에 근거한 표기</Link></li>
-      <li className='text-sm border-b xs:border-b-0 border-solid w-fit'><Link to="/">개인 정보 보호 방침</Link></li>
+      <li className='text-xs border-b xs:border-b-0 border-solid w-fit'><Link to="/">이용 규약과 정책</Link></li>
+      <li className='text-xs border-b xs:border-b-0 border-solid w-fit'><Link to="/">특정 상거래에 관한 볍률에 근거한 표기</Link></li>
+      <li className='text-xs border-b xs:border-b-0 border-solid w-fit'><Link to="/">개인 정보 보호 방침</Link></li>
     </ul>
     <ul className='flex flex-col gap-y-4 absolute top-1/3 right-8'>
       <li><a href="https://www.youtube.com"><img className='mx-auto' width={40} height={40} src="/assets/tiktok.png" /></a></li>
