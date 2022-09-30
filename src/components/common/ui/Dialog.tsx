c6c4a7a28ad7b40dbbca745876eeaed98426fdc4
Kim Myungseong <forwarm5891@gmail.com>
2022-09-30 21:19:02 +0900

    design: 헤더 컴포넌트 width값 수정

--- src/components/common/ui/Dialog.tsx
+++ src/components/common/ui/Dialog.tsx
@@ -12,8 +12,8 @@ const Dialog = ({onClose,children,isModalOpen}:DialogProps) => {
   if(!isModalOpen) return <></>;
 
   return (
-    <div className='absolute top-1/2 left-1/2 xs:top-1/2 xs:left-1/2 -translate-x-1/2 translate-y-1/2 overflow-auto w-[2560px] h-screen bg-gray-300 bg-opacity-40 z-20'>
-    <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex w-[350px] rounded-md xs:w-[400px] bg-lenssisSky">
+    <div className='absolute left-1/2 xs:top-1/2 xs:left-1/2 -translate-x-1/2 translate-y-1/2 overflow-auto w-[2560px] h-screen bg-gray-300 bg-opacity-40 z-20'>
+    <div className="fixed top-[60%] xs:top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex w-[350px] rounded-md xs:w-[400px] bg-lenssisSky">
     <div className="relative w-full max-w-md m-auto flex-col flex rounded-lg ">
     <div className='flex flex-col items-start justify-center'>{children}</div>
     <span className="absolute top-0 right-0 p-4">     
