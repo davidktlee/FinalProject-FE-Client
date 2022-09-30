d368bb3afc48a8ffee399b50403e32d56ecdf20e
chan <cham9994@gmail.com>
2022-09-30 19:14:45 +0900

    fix: 모바일 필터 토글 에러 수정

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,5 @@
-import {useCallback} from 'react'
-import {useLocation } from 'react-router-dom'
+import { useCallback } from 'react'
+import { useLocation } from 'react-router-dom'
 import LeftSection from './LeftSection'
 import RightSection from './RightSection'
 import TopInfomation from './TopInfomation'
@@ -11,12 +11,13 @@ import { useUser } from '../auth/hooks/useUser'
 
 const Header = () => {
   const [filterOpen, setFilterOpen] = useRecoilState(filterState)
-  const {user} = useUser()
+  const { user } = useUser()
   const handleFilter = useCallback(() => {
-    setFilterOpen(!filterOpen)
-  },[])
-  const {pathname} = useLocation();
-  
+    setFilterOpen((prev) => !prev)
+    console.log(filterOpen)
+  }, [])
+  const { pathname } = useLocation()
+
   return (
     <nav className="fixed flex flex-col justify-center bg-[#ABC8DF] text-white z-50 w-full top-0">
       <TopInfomation />
@@ -28,16 +29,15 @@ const Header = () => {
 
         {/* 모바일 navigation */}
         <div className="flex items-center xs:hidden gap-4 pr-2">
-        <button className="flex items-center">
+          <button className="flex items-center">
             <TbSearch size={24} />
           </button>
-          {pathname === "/" &&
-           <button className="flex items-center" onClick={handleFilter}>
-            <FiFilter size={24} />
-          </button>
-          }
+          {pathname === '/' && (
+            <button className="flex items-center" onClick={handleFilter}>
+              <FiFilter size={24} />
+            </button>
+          )}
         </div>
-
       </div>
     </nav>
   )
