dd43d459486f2c2668ce9b5122648ab99bcf5284
chan <cham9994@gmail.com>
2022-10-03 00:09:32 +0900

    feat: 리뷰 페이지 디테일 수정

--- src/pages/ReviewPage.tsx
+++ src/pages/ReviewPage.tsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import PageLayout from '../components/common/ui/PageLayout'
 import ReviewMobileBanner from '../components/review/mobile/ReviewMobileBanner'
 import ReviewBanner from '../components/review/ReviewBanner'
 import ReviewContainer from '../components/review/ReviewContainer'
+import Pagination from '../components/main/common/Pagination'
+import { useState } from 'react'
 
 const ReviewPage = () => {
+  const [allProductCurrentPage, setAllProductCurrentPage] = useState(1)
   return (
     <div>
       <section className="text-gray-600 body-font overflow-hidden">
         <div className="max-w-[1180px] container pt-44 pb-10 mx-auto ">
-          <div className="lg:w-full mx-auto flex-col flex-wrap drop-shadow-basic rounded-[10px] p-8 xs-max:w-[95%] bg-white">
-            <div className="text-center mt-2 mb-6 font-bold text-[20px]">리뷰</div>
+          <div className="lg:w-full mx-auto flex-col flex-wrap shadow-basic rounded-[10px] p-8 xs-max:w-[95%] bg-white">
+            <div className="text-center mt-4 mb-6 font-bold text-[20px] xs-max:pb-20 pb-20">리뷰</div>
             <select
               name="상품명"
               id=""
@@ -25,6 +26,7 @@ const ReviewPage = () => {
               <ReviewMobileBanner />
             </div>
             <ReviewContainer />
+            <Pagination allCount={40} currentPage={1} setCurrentPage={setAllProductCurrentPage} />
           </div>
         </div>
       </section>
