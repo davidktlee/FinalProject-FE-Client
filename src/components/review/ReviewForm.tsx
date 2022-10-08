421bfda58058ac8afd7b0ccf4a9a4e431ec0cf56
chan <cham9994@gmail.com>
2022-10-09 02:42:18 +0900

    feat: reviewForm 완료 - 별점 라이브러리 설치, S3 작업 필요

--- src/components/review/ReviewForm.tsx
+++ src/components/review/ReviewForm.tsx
@@ -1,12 +1,28 @@
+import { useState } from 'react'
 import Button from '../common/Button'
+import ReactStars from 'react-rating-stars-component'
 
 interface ReviewFormProps {
   onClose: Function
   isModalOpen: boolean
+  orderNumber: number
 }
 
-const ReviewForm = ({ onClose, isModalOpen }: ReviewFormProps) => {
+const ReviewForm = ({ onClose, isModalOpen, orderNumber }: ReviewFormProps) => {
+  const [reviewData, setReviewData] = useState()
+  const [reviewText, setReviewText] = useState('')
+  const [rating, setRating] = useState(0)
+  console.log(orderNumber)
+
   if (!isModalOpen) return <></>
+
+  const ratingChanged = (newRating: number) => {
+    setRating(newRating)
+  }
+
+  const handleReviewText = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setReviewText(e.target.value)
+  }
 
   return (
     <>
@@ -14,21 +30,80 @@ const ReviewForm = ({ onClose, isModalOpen }: ReviewFormProps) => {
         <>
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
-              <div className="w-[900px] h-[700px] border-0 rounded-lg shadow-lg relative flex flex-col bg-white outline-none focus:outline-none">
-                <div className="flex items-start justify-center p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">리뷰작성</h3>
+              <div className="xs-max:w-[300px] xs-max:h-[600px] w-[900px] h-[700px] border-0 rounded-lg shadow-lg relative flex flex-col bg-white outline-none focus:outline-none">
+                <div className="flex items-start justify-center pt-[26px] ">
+                  <h3 className="text-[22px] font-semibold">리뷰작성</h3>
+                </div>
+                <div className=" pt-[10px] px-[20px]">
+                  <div className="xs-max:gap-[10px] flex gap-[20px] border-solid border-lenssisStroke border-[1px] rounded-[5px]">
+                    <div className="py-[15px] px-[14px] ">
+                      <label htmlFor="selectImage" className="relative cursor-pointer hover:contrast-[.6]">
+                        <img className="" width={120} height={120} src="/assets/eyes.png" />
+                        <div className="text-[12px] opacity-0 hover:opacity-100 absolute inset-0 z-10 flex justify-center items-center text-white font-semibold">
+                          <span className="text-white px-2 py-1 border-solid border-white border-[2px] rounded-[5px]">
+                            사진 첨부하기
+                          </span>
+                        </div>
+                      </label>
+                      <input type="file" name="image" id="selectImage" hidden={true} />
+                    </div>
+                    <div className="flex flex-col py-[25px] justify-between">
+                      <div className="xs-max:text-[12px] text-[14px]">샌드 플러스 그레이</div>
+                      <div className="xs-max:text-[10px] text-[12px] text-lenssisGray">옵션 선택</div>
+                      <div className="xs-max:text-[10px] text-[12px] text-lenssisGray">가격</div>
+                      <div className="xs:hidden flex gap-[2px]">
+                        <ReactStars
+                          count={5}
+                          onChange={ratingChanged}
+                          size={18}
+                          isHalf={true}
+                          emptyIcon={<i className="far fa-star"></i>}
+                          halfIcon={<i className="fa fa-star-half-alt"></i>}
+                          fullIcon={<i className="fa fa-star"></i>}
+                          color="#efefef"
+                          activeColor="#ffd700"
+                        />
+                      </div>
+                      <div className="xs-max:hidden flex gap-[2px]">
+                        <ReactStars
+                          count={5}
+                          onChange={ratingChanged}
+                          size={24}
+                          isHalf={true}
+                          emptyIcon={<i className="far fa-star"></i>}
+                          halfIcon={<i className="fa fa-star-half-alt"></i>}
+                          fullIcon={<i className="fa fa-star"></i>}
+                          color="#efefef"
+                          activeColor="#ffd700"
+                        />
+                      </div>
+                    </div>
+                  </div>
                 </div>
 
-                <form className="relative p-6 flex-auto">
-                  <textarea cols={70} rows={10}></textarea>
-                </form>
+                <div className="w-full mx-auto pb-[20px] px-[20px] pt-[15px]">
+                  <textarea
+                    placeholder="리뷰를 입력해 주세요. (최소 100자)"
+                    value={reviewText}
+                    onChange={(e) => handleReviewText(e)}
+                    className="xs-max:w-[260px] xs-max:h-[260px] text-[14px] box-border px-[12px] py-[14px] w-[860px] h-[335px] outline-none resize-none rounded-[5px] border-solid border-[1px] border-lenssisStroke"
+                  ></textarea>
+                </div>
 
-                <div className="flex items-center justify-center gap-2 p-6 border-t border-solid border-slate-200 rounded-b">
-                  <Button onClick={() => onClose()} bgColor="white">
+                <div className="xs:hidden flex items-center justify-center gap-[10px]">
+                  <Button onClick={() => onClose()} bgColor="white" width="w-[120px]">
                     <span>취소</span>
                   </Button>
-                  <Button onClick={() => onClose()} bgColor="dark">
-                    <span>작성</span>
+                  <Button onClick={() => onClose()} bgColor="dark" width="w-[120px]">
+                    <span>저장</span>
+                  </Button>
+                </div>
+                <div className="xs-max:hidden flex items-center justify-center gap-[10px]">
+                  <Button onClick={() => onClose()} bgColor="white" width="w-[150px]">
+                    <span>취소</span>
+                  </Button>
+                  <Button onClick={() => onClose()} bgColor="dark" width="w-[150px]">
+                    <span>저장</span>
                   </Button>
                 </div>
               </div>
